9678134392b725246b4541e487b4d8fe10f9a917
nickeny <nickeny@yandex.ru>
2021-04-02 21:54:40 +0300

    change access-control

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json())
 app.use(
   function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://test-for-careerist.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
